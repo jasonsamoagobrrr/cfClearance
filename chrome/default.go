d0260425816a937811c5cd6596ba8b50ff383538
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2020-10-02 18:36:52 -0400

    push chromedp deps and operations into its own package

---
+++ chrome/default.go
@@ -0,0 +1,61 @@
+package chrome
+
+import (
+	"context"
+	"errors"
+	"github.com/chromedp/chromedp"
+	"gitlab.com/gitlab-com/gl-security/security-operations/gl-redteam/cfClearance/cfclient"
+	"log"
+	"net/http"
+	"time"
+)
+
+func GetCloudFlareClearanceCookie(client *http.Client, agent string, target string) (*http.Client, error) {
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		// Ignore certificate errors (for use with proxy testing)
+		chromedp.Flag("ignore-certificate-errors", "1"),
+		// User-Agent MUST match what your tooling uses
+		chromedp.UserAgent(agent),
+	)
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
+
+	// Create the chrome instance
+	ctx, cancel := chromedp.NewContext(
+		allocCtx,
+		chromedp.WithLogf(log.Printf),
+	)
+	defer cancel()
+
+	// Challenges should be solved in ~5 seconds but can be slower. Timeout at 30.
+	ctx, cancel = context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	// Listen for the Cloudflare cookie
+	cookieReceiverChan := make(chan string, 1)
+	defer close(cookieReceiverChan)
+
+	// Fetch the login page and wait until CF challenge is solved.
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(target),
+		chromedp.WaitNotPresent(`Checking your browser`, chromedp.BySearch),
+		cfclient.ExtractCookie(cookieReceiverChan),
+	)
+	if err != nil {
+		if err == context.DeadlineExceeded {
+			return client, errors.New("Context deadline exceeded trying to grab cookie using chromedp")
+		}
+		return client, err
+	}
+
+	// block the program until the cloud flare cookie is received, or .WaitVisible times out looking for login-pane
+	cfToken := <-cookieReceiverChan
+
+	log.Printf("[*] Grabbed Cloudflare token: %s", cfToken)
+
+	// Finally, build up the cookie jar with the required token
+	cookieURL, cookies := cfclient.BakeCookies(target, cfToken)
+	client.Jar.SetCookies(cookieURL, cookies)
+
+	return client, nil
+}
--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package cfClearance
 
 import (
-	"context"
 	"errors"
 	"gitlab.com/gitlab-com/gl-security/security-operations/gl-redteam/cfClearance/cfclient"
+	"gitlab.com/gitlab-com/gl-security/security-operations/gl-redteam/cfClearance/chrome"
 	"gitlab.com/gitlab-com/gl-security/security-operations/gl-redteam/cfClearance/validate"
 	"log"
 	"net/http"
-	"time"
-
-	"github.com/chromedp/chromedp"
 )
 
 // MakeCfClient should be called directly, using a URL string as the target
@@ -35,52 +32,7 @@ func MakeCfClient(target string, agent string) (*http.Client, error) {
 
 	log.Println("[!] Target is protected by Cloudflare, bypassing...")
 
-	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		// Ignore certificate errors (for use with proxy testing)
-		chromedp.Flag("ignore-certificate-errors", "1"),
-		// User-Agent MUST match what your tooling uses
-		chromedp.UserAgent(agent),
-	)
-	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
-	defer cancel()
+	return chrome.GetCloudFlareClearanceCookie(client, agent, target)
 
-	// Create the chrome instance
-	ctx, cancel := chromedp.NewContext(
-		allocCtx,
-		chromedp.WithLogf(log.Printf),
-	)
-	defer cancel()
-
-	// Challenges should be solved in ~5 seconds but can be slower. Timeout at 30.
-	ctx, cancel = context.WithTimeout(ctx, 10*time.Second)
-	defer cancel()
-
-	// Listen for the Cloudflare cookie
-	cookieReceiverChan := make(chan string, 1)
-	defer close(cookieReceiverChan)
-
-	// Fetch the login page and wait until CF challenge is solved.
-	err := chromedp.Run(ctx,
-		chromedp.Navigate(target),
-		chromedp.WaitNotPresent(`Checking your browser`, chromedp.BySearch),
-		cfclient.ExtractCookie(cookieReceiverChan),
-	)
-	if err != nil {
-		if err == context.DeadlineExceeded {
-			return client, errors.New("Context deadline exceeded trying to grab cookie using chromedp")
-		}
-		return client, err
-	}
-
-	// block the program until the cloud flare cookie is received, or .WaitVisible times out looking for login-pane
-	cfToken := <-cookieReceiverChan
-
-	log.Printf("[*] Grabbed Cloudflare token: %s", cfToken)
-
-	// Finally, build up the cookie jar with the required token
-	cookieURL, cookies := cfclient.BakeCookies(target, cfToken)
-	client.Jar.SetCookies(cookieURL, cookies)
-
-	return client, nil
 }
 
